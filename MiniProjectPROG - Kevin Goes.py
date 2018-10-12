32e530096d15a143d5c1782e4a77c63a17cb05df
Kevin Goes <kevin.goes@student.hu.nl>
2018-10-12 12:14:41 +0200

    Miniproject - Kevin Goes

--- MiniProjectPROG - Kevin Goes.py
+++ MiniProjectPROG - Kevin Goes.py
@@ -42,24 +42,26 @@ def infoPubliek():
 
 def infoPersoonlijk():
     aantalRegels, stallingBezet = infoPubliek()
-    invoerStalling = input('Welke stalling heeft u? ')
-    if invoerStalling in stallingBezet:
-        invoerWachtwoord = input('Welk wachtwoord heeft u voor stalling {}? '.format(invoerStalling))
-        with open('stalling.csv', 'r', newline='') as myCSVFile:
-            aantalRegels = 0
-            wachtwoord = []
-            lezer = csv.DictReader(myCSVFile, delimiter=';')
-            for regel in lezer:
-                aantalRegels += 1
-                omgezet = terugomzettenASCII(regel['t^`eqtlloa'])
-                wachtwoord.append(omgezet)
-            regel = stallingBezet.index(invoerStalling)
-            if wachtwoord[regel] == invoerWachtwoord:
-                return 'U heeft toegang tot uw stalling!'
-            else:
-                return 'Wachtwoord Fout!'
-    else:
-        return 'Stalling is niet in bezet'
+    while True:
+        invoerStalling = input('Welke stalling heeft u? ')
+        if invoerStalling in stallingBezet:
+            while True:
+                invoerWachtwoord = input('Welk wachtwoord heeft u voor stalling {}? '.format(invoerStalling))
+                with open('stalling.csv', 'r', newline='') as myCSVFile:
+                    aantalRegels = 0
+                    wachtwoord = []
+                    lezer = csv.DictReader(myCSVFile, delimiter=';')
+                    for regel in lezer:
+                        aantalRegels += 1
+                        omgezet = terugomzettenASCII(regel['t^`eqtlloa'])
+                        wachtwoord.append(omgezet)
+                    regel = stallingBezet.index(invoerStalling)
+                    if wachtwoord[regel] == invoerWachtwoord:
+                        return 'U heeft toegang tot uw stalling!'
+                    else:
+                        print('Verkeerd wachtwoord voor stalling {}, probeer het nogmaals \n'.format(invoerStalling))
+        else:
+            print('Stalling {} is niet in bezet, probeer het nogmaals \n'.format(invoerStalling))
 
 
 vrij, stalling = infoPubliek()
