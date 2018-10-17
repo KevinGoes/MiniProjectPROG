ee69157d3f6efe5462ad99868d37678a5425d25f
Kevin Goes <kevin.goes@student.hu.nl>
2018-10-17 11:57:13 +0200

    MiniprojectCSN - Kevin Goes

--- MiniProjectPROG - Kevin Goes.py
+++ MiniProjectPROG - Kevin Goes.py
@@ -29,14 +29,13 @@ def terugomzettenASCII(omgezetteWaarde):
 
 def infoPubliek():
     with open('stalling.csv', 'r', newline='') as myCSVFile:
-        lezer = csv.DictReader(myCSVFile, delimiter=';')
+        lezer = csv.DictReader(myCSVFile, delimiter=',')
         aantalRegels = 0
         stallingBezet = []
         for regel in lezer:
             aantalRegels += 1
-            omgezet = terugomzettenASCII(regel['pq^ifkd'])
+            omgezet = terugomzettenASCII(regel['pq^iifkd'])
             stallingBezet.append(omgezet)
-        aantalRegels = 100 - aantalRegels
         return aantalRegels, stallingBezet
 
 
@@ -50,7 +49,7 @@ def infoPersoonlijk():
                 with open('stalling.csv', 'r', newline='') as myCSVFile:
                     aantalRegels = 0
                     wachtwoord = []
-                    lezer = csv.DictReader(myCSVFile, delimiter=';')
+                    lezer = csv.DictReader(myCSVFile, delimiter=',')
                     for regel in lezer:
                         aantalRegels += 1
                         omgezet = terugomzettenASCII(regel['t^`eqtlloa'])
@@ -61,7 +60,7 @@ def infoPersoonlijk():
                     else:
                         print('Verkeerd wachtwoord voor stalling {}, probeer het nogmaals \n'.format(invoerStalling))
         else:
-            print('Stalling {} is niet in gebruik, probeer het nogmaals \n'.format(invoerStalling))
+            print('Stalling {} is niet in bezet, probeer het nogmaals \n'.format(invoerStalling))
 
 
 vrij, stalling = infoPubliek()
